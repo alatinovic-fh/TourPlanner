818f0dc66bec157925051182ce6f20c7cd8d5f48
alatinovic-fh <if24b050@technikum-wien.at>
2025-05-13 16:38:39 +0200

    fix: removed unused class from Tour

--- src/main/java/at/fh/bif/swen/tourplanner/model/Tour.java
+++ src/main/java/at/fh/bif/swen/tourplanner/model/Tour.java
@@ -1,36 +1,8 @@
 package at.fh.bif.swen.tourplanner.model;
 
+// TODO implement full Tour record
+
 public record Tour(String name, String description, String from, String to){
-    @Override
-    public String toString() {
-        return "Tour [name=" + name + ", description=" + description + ", from=" + from+ ", to=" + to + "]";
-    }
-}
-
-class TourAsClass{
-    private String name;
-    private String description;
-    private String from;
-    private String to;
-    //TODO full Tour object
-
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
     @Override
     public String toString() {
         return "Tour [name=" + name + ", description=" + description + ", from=" + from+ ", to=" + to + "]";
