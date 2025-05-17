31a0c0d4ee4e0727a73c2295b01249b1ccbc2c66
alatinovic-fh <if24b050@technikum-wien.at>
2025-05-17 15:52:41 +0200

    feat: add tour attributes to record and Toolbar to UI

--- src/main/java/at/fh/bif/swen/tourplanner/model/Tour.java
+++ src/main/java/at/fh/bif/swen/tourplanner/model/Tour.java
@@ -1,8 +1,10 @@
 package at.fh.bif.swen.tourplanner.model;
 
-// TODO implement full Tour record
+// TODO implement full Tour record + Add route Information
 
-public record Tour(String name, String description, String from, String to){
+import java.sql.Time;
+
+public record Tour(String name, String description, String from, String to, TransportType type, double distance, Time estimatedTime){
     @Override
     public String toString() {
         return name;
